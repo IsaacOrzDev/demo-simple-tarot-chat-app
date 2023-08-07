e195f3bba4981ea1ff4f57658ea2b678284ba08e
Isaac <isaachkdeveloper@gmail.com>
2023-08-07 11:49:56 +0800

    feat: Accept multiple text files

--- src/chat.py
+++ src/chat.py
@@ -8,13 +8,16 @@ from vector_db_restore import vector_db
 llm = Cohere(temperature=0.5)
 # llm = OpenAI(temperature=0.5)
 
-drawing_result = "if I draw a card with the fool in upright position"
+drawing_result = "if I draw a card with the magician in upright position"
+# "if I draw a card with the fool in upright position"
 
 summary_prompt_template = """
 Please summarize the last question that the human want to ask base on the chat history with AI.
 The questions from human should related to Tarot cards and drawing result, if some questions from human are not related to Tarot cards and drawing result, please ignore them.
 Response with the last question from human only, no other words, also please do not answer the question.
-Chat History: {chat_history}"""
+Chat History: 
+{chat_history}
+"""
 summary_prompt = PromptTemplate(
     input_variables=["chat_history"], template=summary_prompt_template)
 summary_chain = LLMChain(llm=llm, prompt=summary_prompt, verbose=False)
@@ -25,12 +28,12 @@ qa = RetrievalQA(combine_documents_chain=qa_chain,
                  retriever=vector_db.as_retriever(), verbose=False)
 
 answer_prompt_template = """
-You are a fortune teller AI who know about Tarot, the human will ask question with the drawing result of Tarot, please answer them with a tone like mystery man.
+You are a Tarot reader AI who know about Tarot, the human will ask question with the drawing result of Tarot, please answer them with a tone like mystery man.
 Read the context and search result, but focus on more what the questions is about, also please try not use the words from context to answer directly, summarize shortly with your own words and thought to conclude in one sentence as possible.
 If the context and search result cannot help you to answer the question, please use your own thought to answer.
 Please do not repeat the answer you have mention before.
 If the context is nothing can help you, please use your own thought to answer.
-If the question is completely not related to Tarot cards and Tarot result, please answer 'I cannot tell you'.
+If the question is completely not related to Tarot cards and Tarot result, please answer 'I am a Tarot reader, and I can only answer questions related to the Tarot cards'.
 Context: 
 {context}
 Human: 
@@ -43,7 +46,7 @@ answer_chain = LLMChain(llm=llm, prompt=answer_prompt, verbose=True)
 
 
 def chat(input, conversations):
-    conversations_str = "";
+    conversations_str = ""
     for conversation in conversations:
         conversations_str += f"{conversation['role']} : {conversation['content']}\n"
     # print(conversations_str)
@@ -51,6 +54,7 @@ def chat(input, conversations):
     # print(question)
     context = qa.run(question)
     context += qa.run(drawing_result)
-    response = answer_chain.run({"question": question, "context": context, "drawing_result": drawing_result})
+    response = answer_chain.run(
+        {"question": question, "context": context, "drawing_result": drawing_result})
     # print(response)
     return response
--- src/vector_db_persist.py
+++ src/vector_db_persist.py
@@ -3,19 +3,25 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from chromadb.utils import embedding_functions
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+import os
 
-loader = TextLoader('./src/data/cards.txt')
-documents = loader.load()
+path = './src/data'
+all_texts = []
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
-
+for filename in os.listdir(path):
+    file_path = f"{path}/{filename}"
+    print(file_path)
+    loader = TextLoader(file_path)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    all_texts = all_texts + texts
 
 embedding_functions = SentenceTransformerEmbeddings(
     model_name="all-MiniLM-L6-v2")
 
 vector_db = Chroma.from_documents(
-    texts, embedding_functions, persist_directory="db")
+    all_texts, embedding_functions, persist_directory="db")
 
 vector_db.persist()
